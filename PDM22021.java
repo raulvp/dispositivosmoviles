5b888456b1eb461cc305e0b83117cbe603a4eafb
Raul Vera Portanda <raulvp9@gmail.com>
2021-04-20 22:52:14 -0400

    Create PDM22021.java

---
+++ PDM22021.java
@@ -0,0 +1,62 @@
+
+package pdm22021;
+
+public class PDM22021 {
+
+    public static void main(String[] args) {
+        
+        /****contador de pares*****/
+        int numero=0;
+        while(numero<=20){
+            if(numero%2==0){
+                System.out.println("Numero par: "+numero);
+            }
+            numero++;
+        }
+        
+        String palabra = "hola";
+        palabra.charAt(2);
+        
+        /*int edad = 0;
+        
+        while(edad>21){
+            System.out.println("Usted es mayor de edad "+edad);
+            edad--;
+        }
+        System.out.println("----------do while---------------");
+        //do while
+        do{
+            System.out.println("Usted tiene la edad de: "+edad);
+            edad--;
+        }while(edad>21);
+        
+        System.out.println("------switch--------");
+        int dia=6;
+        switch(dia){
+            case 1: System.out.println("Lunes");
+            break;
+            case 2: System.out.println("Martes");
+            break;
+            case 3: System.out.println("Miercoles");
+            break;
+            case 4: System.out.println("Jueves");
+            break;
+            case 5: System.out.println("Viernes");
+            break;
+            default: System.out.println("Error en el dia ingresado");
+            break;
+        }
+        
+        String mes= "Enero";
+        switch(mes){
+            case "Enero": System.out.println("Este mes es Enero");
+            break;
+            case "Febrero": System.out.println("Este mes es febrero");
+            break;
+        }
+        
+        System.out.println("------------------");
+     */   
+    }
+    
+}
